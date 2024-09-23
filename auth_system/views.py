078b39631a3728c51135b12a2a6942769fb8bee3
Nikhils9346 <nikhilabeereddy@gmail.com>
2024-09-23 14:20:52 +0530

    first commit

---
+++ auth_system/views.py
@@ -0,0 +1,49 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, logout, login
+
+# Create your views here.
+
+def signup(request):
+    if request.method == 'POST':
+        user = request.POST['username']
+        email = request.POST['email']
+        p1 = request.POST['password1']
+        p2 = request.POST['password2']
+
+        if p1 == p2:
+            if User.objects.filter(username = user).exists():
+                context={
+                    'error': "USER ALREADY EXISTS"
+                }
+                return render(request, 'signup.html', context)
+            else:
+                User.objects.create_user(
+                    username=user,
+                    email = email,
+                    password=p1
+                )
+                return redirect('login')
+    return render(request, 'signup.html')
+    
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        p = request.POST['password']
+
+        user = authenticate(request, username = username, password = p)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponse("HELLO WELCOME")
+        
+
+    return render(request, 'login.html')
+    
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+    
